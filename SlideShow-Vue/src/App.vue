6f0b81b25ed87a741cbfa60e4ef3162717bac253
JoshRobs <jdr.joshuaroberts@gmail.com>
2025-06-18 21:54:51 -0300

    Added title to page

--- SlideShow-Vue/src/App.vue
+++ SlideShow-Vue/src/App.vue
@@ -12,20 +12,24 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen">
-    <div class="w-full max-w-full aspect-video flex items-center justify-center">
-      <!--Keep aspect ratio in wrapper to keep button locked below-->
-      <ImageSlideshow v-if="!isLoading" :images="images" :autoplay="true" :interval="6000" />
-      <pulse-loader v-else :loading="isLoading" :color="color" :size="size"></pulse-loader>
+  <div class="min-h-screen">
+    <h1 class="text-xl">Tracker Network Carousel</h1>
+    <h2>Author: Joshua Roberts</h2>
+    <div class="flex flex-col items-center justify-center">
+      <div class="w-full max-w-full aspect-video flex items-center justify-center">
+        <!--Keep aspect ratio in wrapper to keep button locked below-->
+        <ImageSlideshow v-if="!isLoading" :images="images" :autoplay="true" :interval="6000" />
+        <pulse-loader v-else :loading="isLoading" :color="color" :size="size"></pulse-loader>
+      </div>
+      <button
+        type="button"
+        class="cursor-pointer w-[50%] text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-xl text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:cursor-default disabled:bg-gray-600 disabled:hover:bg-gray-600 transition-colors duration-400 ease-in-out"
+        :disabled="isLoading"
+        @click="fetchImages()"
+      >
+        Load New Images
+      </button>
     </div>
-    <button
-      type="button"
-      class="cursor-pointer w-[50%] text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-xl text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:cursor-default disabled:bg-gray-600 disabled:hover:bg-gray-600 transition-colors duration-400 ease-in-out"
-      :disabled="isLoading"
-      @click="fetchImages()"
-    >
-      Load New Images
-    </button>
   </div>
 </template>
 
